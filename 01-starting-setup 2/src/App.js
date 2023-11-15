7d0b256a7477527a76c907a98788b6636ff49f9d
jumosd <jumosd@icloud.com>
2023-11-16 07:07:36 +0900

    헤더컴포넌트분리

--- 01-starting-setup 2/src/App.js
+++ 01-starting-setup 2/src/App.js
@@ -1,7 +1,8 @@
-import keyConceptsImage from './assets/images/key-concepts.png';
+
 import componentsImage from './assets/images/components.png';
 import stateImage from './assets/images/state.png';
 import eventsImage from './assets/images/events.png';
+import Header from './components/Header/Header';
 
 const concepts = [
   {
@@ -27,17 +28,33 @@ const concepts = [
 function App() {
   return (
     <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
+      <Header></Header>
       <ul id="concepts">
         <li className="concept">
           <img src={concepts[0].image} alt={concepts[0].title} />
           <h2>{concepts[0].title}</h2>
           <p>{concepts[0].description}</p>
         </li>
+        <li className="concept">
+          <img src={concepts[1].image} alt={concepts[1].title} />
+          <h2>{concepts[1].title}</h2>
+          <p>{concepts[1].description}</p>
+        </li>
+        <li className="concept">
+          <img src={concepts[2].image} alt={concepts[2].title} />
+          <h2>{concepts[2].title}</h2>
+          <p>{concepts[2].description}</p>
+        </li>
+
+        {concepts.map((item) => {
+          return (
+            <li className="concept">
+              <img src={item.image} alt={item.title} />
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+            </li>
+          )
+        })}
       </ul>
     </div>
   );
