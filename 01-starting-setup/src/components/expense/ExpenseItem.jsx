e2af8c10cbbd9dae6bc9803fa129b707ed5db718
하방방 <81242485+jumosd@users.noreply.github.com>
2023-12-15 19:25:50 +0900

    useState()

--- 01-starting-setup/src/components/expense/ExpenseItem.jsx
+++ 01-starting-setup/src/components/expense/ExpenseItem.jsx
@@ -1,15 +1,15 @@
+import { useState } from 'react'
 import ExpenseDate from './ExpenseDate';
-
 import './ExpenseItem.css'
 import Card from '../ui/Card';
 
 function ExpenseItem(props) {
-
+    const [title, setTitle] = useState(props.title)
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
         </Card>);
